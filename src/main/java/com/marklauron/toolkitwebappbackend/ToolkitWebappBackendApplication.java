58874bae9172b3055f203505a087487c3fa793c3
Mark <malauron@gmail.com>
2020-07-05 20:38:44 +0800

    Initial Commit

---
+++ src/main/java/com/marklauron/toolkitwebappbackend/ToolkitWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marklauron.toolkitwebappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolkitWebappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolkitWebappBackendApplication.class, args);
+    }
+
+}
